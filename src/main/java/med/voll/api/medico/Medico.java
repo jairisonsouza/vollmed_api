9ee615fd0a08eccd38fd6f30712fe93fb9073e81
jairisonsouza <jairo.tinformatica@gmail.com>
2023-07-27 16:45:16 -0300

    Configuração de db-migrations e validações dos campos

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -19,6 +19,8 @@ import med.voll.api.endereco.Endereco;
         private Long id;
         private String nome;
         private String email;
+
+        private String telefone;
         private String crm;
 
         @Enumerated(EnumType.STRING)
@@ -27,4 +29,12 @@ import med.voll.api.endereco.Endereco;
         @Embedded
         private Endereco endereco;
 
-}
+        public Medico(DadosCadastroMedico dados) {
+            this.nome = dados.nome();
+            this.email = dados.email();
+            this.telefone = dados.telefone();
+            this.crm = dados.crm();
+            this.especialidade = dados.especialidade();
+            this.endereco = new Endereco(dados.endereco());
+        }
+    }
