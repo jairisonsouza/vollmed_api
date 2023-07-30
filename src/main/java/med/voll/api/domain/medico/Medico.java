592a2ec16993c4e24d2c1442e1fe37549dde415f
jairisonsouza <jairo.tinformatica@gmail.com>
2023-07-29 21:40:10 -0300

    Refatorando e tratando erros

--- src/main/java/med/voll/api/domain/medico/Medico.java
+++ src/main/java/med/voll/api/domain/medico/Medico.java
@@ -1,13 +1,13 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
-    @Table(name = "medicos")
+@Table(name = "medicos")
     @Entity(name = "Medico")
     @Getter
     @NoArgsConstructor
